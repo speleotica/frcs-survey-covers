6f2f96af136ce6fde4ce79a9d7eaadf319509353
Andy Edwards <jedwards@fastmail.com>
2024-04-27 09:46:32 -0500

    feat: add cover page generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,94 @@
-import Image from "next/image";
+"use client";
+
+import * as React from "react";
 import styles from "./page.module.css";
+import {
+  FrcsTripSummaryFile,
+  parseFrcsTripSummaryFile,
+} from "@speleotica/frcsdata";
+import { chunk } from "lodash";
 
 export default function Home() {
+  const [statSum, setStatSum] = React.useState("");
+  const [tripSummaryFile, setTripSummaryFile] =
+    React.useState<FrcsTripSummaryFile | null>(null);
+
+  React.useEffect(() => {
+    let canceled = false;
+    async function* getLines() {
+      yield* statSum.split(/\r\n?|\n/gm);
+    }
+    (async () => {
+      setTripSummaryFile(
+        await parseFrcsTripSummaryFile("STAT_sum.txt", getLines())
+      );
+    })();
+    return () => {
+      canceled;
+    };
+  }, [statSum]);
+
+  const summaries = React.useMemo(
+    () =>
+      tripSummaryFile?.tripSummaries?.map((summary) =>
+        summary ? (
+          <div key={summary.tripIndex} className={styles.coverPage}>
+            <div className={styles.tripNumber}>{summary.tripNumber}</div>
+            <div className={styles.date}>
+              {String(summary.date.getMonth() + 1).padStart(2, " ")}/
+              {String(summary.date.getDate()).padStart(2, " ")}/
+              {summary.date.getFullYear()}
+            </div>
+            <div className={styles.tripName}>{summary.name}</div>
+            <div className={styles.stats}>
+              <div className={styles.totalLength}>
+                {summary.totalLength.toString()}
+              </div>
+              <div className={styles.numShots}>{summary.numShots} sta.</div>
+              <div className={styles.excluded}>
+                EXCLUDED:
+                <div className={styles.excludedLength}>
+                  {summary.excludedLength
+                    .get(summary.totalLength.unit)
+                    .toFixed(2)}
+                </div>
+                <div className={styles.numExcludedShots}>
+                  {summary.numExcludedShots}
+                </div>
+              </div>
+            </div>
+            <div className={styles.team}>
+              {summary.team.map((name, index) => (
+                <div key={index}>{name}</div>
+              ))}
+            </div>
+            <table className={styles.shots}>
+              <tbody>
+                {chunk(summary.shots, 4).map((row, index) => (
+                  <tr key={index}>
+                    {row.map((value, index) => (
+                      <td key={index}>{value}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : null
+      ),
+    [tripSummaryFile?.tripSummaries]
+  );
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <textarea
+        className={styles.statSumField}
+        value={statSum}
+        onChange={(e) => setStatSum(e.currentTarget.value)}
+        placeholder="paste STAT_sum.txt here"
+        rows={5}
+      />
+      <div>{summaries}</div>
     </main>
   );
 }
